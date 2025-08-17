de723e25488371f7970e20ef2b4d94cbfb95e353
Valaban <valaban.info@gmail.com>
2025-08-18 00:34:15 +0100

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1755473649765

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/node_modules_355b6f47._.js",
+  "static/chunks/src_f0c59f69._.js"
+],
+    source: "dynamic"
+});
